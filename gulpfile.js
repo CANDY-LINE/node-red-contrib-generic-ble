f9b25604168b18d5d2b89df8a1bda49e56f6fdde
Daisuke Baba <baba.daisuke@gmail.com>
2017-07-02 12:44:46 +0900

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+const gulp        = require('gulp');
+const util        = require("gulp-util");
+const babel       = require('gulp-babel');
+const browserify  = require('browserify');
+const babelify    = require('babelify');
+const uglify      = require('gulp-uglify');
+const del         = require('del');
+const source      = require('vinyl-source-stream');
+const buffer      = require('vinyl-buffer');
+const jshint      = require('gulp-jshint');
+const mocha       = require('gulp-mocha');
+const sourcemaps  = require('gulp-sourcemaps');
+const gulpif      = require('gulp-if');
+const htmlmin     = require('gulp-htmlmin');
+const cleancss    = require('gulp-clean-css');
+const less        = require('gulp-less');
+const manifest    = require('gulp-manifest');
+const shim        = require('browserify-shim');
+const yaml        = require('gulp-yaml');
+
+const minified = process.env.NODE_ENV === 'production';
+const sourcemapEnabled = process.env.NODE_ENV !== 'production';
+
+gulp.task('lint', () => {
+  return gulp.src([
+    './tests/**/*.js',
+    './src/**/*.js'
+  ])
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('clean', () => {
+  return del([
+    'dist/*',
+    './dist',
+    '!node_modules/**/*',
+    './*.tgz',
+  ]);
+});
+
+gulp.task('i18n', () => {
+  return gulp.src([
+      './src/locales/**/*.{yaml,yml}'
+    ])
+    .pipe(yaml({ safe: true }))
+    .pipe(gulp.dest('./dist/locales'));
+});
+
+gulp.task('assets', ['i18n'], () => {
+  return gulp.src([
+      './src/**/*.{less,ico,png,json,yaml,yml}',
+      '!./src/locales/**/*.{yaml,yml}'
+    ])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', ['assets'], () => {
+  return browserify({
+      entries: './src/generic-ble.js',
+      debug: true
+    })
+    .transform(babelify, {
+      minified: minified,
+      compact: minified,
+      presets: ["es2015"],
+      plugins: ['add-module-exports'],
+      sourceMaps: sourcemapEnabled,
+    }).on('error', util.log)
+    .transform(shim, {
+      global: true
+    }).on('error', util.log)
+    .bundle()
+    .pipe(source('generic-ble.js'))
+    .pipe(buffer())
+    .pipe(gulpif(sourcemapEnabled, sourcemaps.init({loadMaps: true}), util.noop()))
+    .pipe(uglify({
+      mangle: minified,
+      compress: {
+        dead_code: true,
+        drop_debugger: true,
+        properties: true,
+        unused: true,
+        toplevel: true,
+        if_return: true,
+        drop_console: !sourcemapEnabled,
+        conditionals: true,
+        unsafe_math: true,
+        unsafe: true
+      },
+    }))
+    .pipe(gulpif(sourcemapEnabled, sourcemaps.write(), util.noop()))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('less', () => {
+  return gulp.src('./src/**/*.less')
+    .pipe(gulpif(sourcemapEnabled, sourcemaps.init(), util.noop()))
+    .pipe(less())
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulpif(sourcemapEnabled, sourcemaps.write(), util.noop()))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('html', () => {
+  return gulp.src('./src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace:true, conservativeCollapse:true}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['lint', 'js', 'less', 'html', 'assets'], () => {
+  gulp.src(['./dist/*'], { base: './dist' })
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('testAssets', () => {
+  return gulp.src('./tests/**/*.{css,less,ico,png,html,json,yaml,yml}')
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('testJs', ['build'], () => {
+  return gulp.src('./tests/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015'],
+      plugins: ['add-module-exports']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('test', ['testJs', 'testAssets'], () => {
+  return gulp.src([
+    './dist/**/*.test.js',
+  ], {read: false})
+  .pipe(mocha({
+    require: ['source-map-support/register'],
+    reporter: 'spec'
+  }))
+  .once('error', () => process.exit(1))
+  .once('end', () => process.exit())
+});
+
+gulp.task('default', ['build']);
