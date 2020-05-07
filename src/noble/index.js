76c09ab04aa491bc0b12d509db97928610a7e8a6
dbaba <baba.daisuke@gmail.com>
2020-05-07 07:35:14 +0000

    Add noble wrapper stuff

---
+++ src/noble/index.js
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright (c) 2020 CANDY LINE INC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Noble from '../../node_modules/@abandonware/noble/lib/noble';
+import os from 'os';
+import cproc from 'child_process';
+import debugLogger from 'debug';
+
+const debug = debugLogger('node-red-contrib-generic-ble:noble');
+
+const platform = os.platform();
+debug(`Detected Platform => [${platform}]`);
+
+let bindings;
+if (platform === 'linux') {
+  try {
+    debug(`Locating bluetoothctl...`);
+    const pathToBluetoothctl = cproc.execSync('which bluetoothctl');
+    bindings = require('./lib/bluetoothctl/bindings').default;
+    debug(`Found => ${pathToBluetoothctl}`);
+  } catch (_) {
+    debug(`bluetoothctl is missing in the current path.`);
+  }
+}
+if (!bindings) {
+  debug(`Loading the default resolve-bindings module in @abandonware/noble.`);
+  bindings = require('../../node_modules/@abandonware/noble/lib/resolve-bindings')();
+}
+
+module.exports = new Noble(bindings);
